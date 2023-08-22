c319e3d4703189f61532369b69b30f6f4159dc00
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-08-23 00:52:29 +0500

    ✨ Initial commit

---
+++ Day1/index.ts
@@ -0,0 +1,10 @@
+// Copyright (c) 2023 author Shakhbozbek Usmonov Miracle Programmer
+// Day 1 Condingbat Warmup 1
+
+// function sleepIn(weekday: boolean, vacation: boolean): boolean {
+//     return (!weekday && !vacation) || (!weekday && vacation);
+// }
+
+// console.log(sleepIn(false, false)); // true
+// console.log(sleepIn(true, false)); // false
+// console.log(sleepIn(false, true)); // true
