53b6ccebc7296224e6e856b252013131bf5e7d78
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-08-24 13:23:38 +0500

    ✅ icyHot and Day3 end

--- Day3/index.js
+++ Day3/index.js
@@ -117,4 +117,14 @@ function startHi(str) {
 // console.log(startHi("hi")); // true
 // console.log(startHi("hello hi")); // false
 // ------------------------------------------
+// icyHot(120, -1) → true
+// icyHot(-1, 120) → true
+// icyHot(2, 120) → false
+function icyHot(temp1, temp2) {
+    return (temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0);
+}
+// console.log(icyHot(120, -1)); // true
+// console.log(icyHot(-1, 120)); // true
+// console.log(icyHot(2, 120)); // false
+// ------------ End ------------
 //# sourceMappingURL=index.js.map
--- Day3/index.ts
+++ Day3/index.ts
@@ -161,3 +161,17 @@ function startHi(str: string): boolean {
 // console.log(startHi("hello hi")); // false
 
 // ------------------------------------------
+
+// icyHot(120, -1) → true
+// icyHot(-1, 120) → true
+// icyHot(2, 120) → false
+
+function icyHot(temp1: number, temp2: number): boolean {
+    return (temp1 < 0 && temp2 > 100) || (temp1 > 100 && temp2 < 0);
+}
+
+// console.log(icyHot(120, -1)); // true
+// console.log(icyHot(-1, 120)); // true
+// console.log(icyHot(2, 120)); // false
+
+// ------------ End ------------
