367c319599e3fcc256843ad4dcb8253d1552c105
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-08-25 23:04:47 +0500

    ☄️ Day4 started and in1020 done

---
+++ Day4/index.ts
@@ -0,0 +1,17 @@
+// Copyright (c) 2023 author Shakhbozbek Usmonov Miracle Programmer
+// Day 4 Condingbat Warmup 1
+// ---------- Started ----------
+
+// in1020(12, 99) → true
+// in1020(21, 12) → true
+// in1020(8, 99) → false
+
+function in1020(a: number, b: number): boolean {
+    return (a >= 10 && a <= 20) || (b >= 10 && b <= 20);
+}
+
+// console.log(in1020(12, 99)); // true
+// console.log(in1020(21, 12)); // true
+// console.log(in1020(8, 99)); // false
+
+// ---------------------------------------------------
