6c0e7b09d25faf018bb88739d9fe2f36b233ca69
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-08-29 12:10:40 +0500

    ⚡️ Day 8 started and helloName done

---
+++ Day8/index.js
@@ -0,0 +1,12 @@
+"use strict";
+// Copyright (c) 2023 author Shakhbozbek Usmonov Miracle Programmer
+// Day 8 CondingJS String 1
+// ---------- Started ----------
+// helloName('Bob') → Hello Bob!
+// helloName('Alice') → Hello Alice!
+// helloName('X') → Hello X!
+function helloName(name) {
+    return `Hello ${name}!`;
+}
+//-----------------------------------------
+//# sourceMappingURL=index.js.map
---
+++ Day8/index.ts
@@ -0,0 +1,14 @@
+// Copyright (c) 2023 author Shakhbozbek Usmonov Miracle Programmer
+// Day 8 CondingJS String 1
+
+// ---------- Started ----------
+
+// helloName('Bob') → Hello Bob!
+// helloName('Alice') → Hello Alice!
+// helloName('X') → Hello X!
+
+function helloName(name: string): string {
+    return `Hello ${name}!`;
+}
+
+//-----------------------------------------
