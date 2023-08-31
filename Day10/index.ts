5f39bab0298a846152cc4bf32e79f0fdb95b71e7
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-08-31 15:40:57 +0500

    🔥 Day 10 started and endOther done

---
+++ Day10/index.ts
@@ -0,0 +1,24 @@
+// Copyright (c) 2023 author Shakhbozbek Usmonov Miracle Programmer
+// Day 10 CondingJS String 2
+
+// ---------- Started ----------
+
+// endOther('Hiabc', 'abc') → true
+// endOther('AbC', 'HiaBc') → true
+// endOther('abc', 'abXabc') → true
+
+function endOther(a: string, b: string): boolean {
+    if (a.length > b.length)
+        return (
+            a.toLowerCase().slice(-b.length) ===
+            b.toLowerCase().slice(-b.length)
+        );
+    if (a.length < b.length)
+        return (
+            a.toLowerCase().slice(-a.length) ===
+            b.toLowerCase().slice(-a.length)
+        );
+    return a.toLowerCase() === b.toLowerCase();
+}
+
+//----------------------------------------------------------------
