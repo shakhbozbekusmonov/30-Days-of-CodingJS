a4303b5e0634e28094fa652cd4cc2219c41111c4
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-09-02 22:33:40 +0500

    🔥 Day 12 started and oneTwo done

---
+++ Day12/index.js
@@ -0,0 +1,16 @@
+"use strict";
+// Copyright (c) 2023 author Shakhbozbek Usmonov Miracle Programmer
+// Day 12 CondingJS String 2
+// ---------- Started ----------
+// oneTwo('abc') → bca
+// oneTwo('tca') → cat
+// oneTwo('tcagdo') → catdog
+function oneTwo(str) {
+    let newStr = "";
+    for (let i = 0; i < str.length - 2; i += 3) {
+        newStr += str.slice(i + 1, i + 3) + str.charAt(i);
+    }
+    return newStr;
+}
+//----------------------------------------------------------------
+//# sourceMappingURL=index.js.map
