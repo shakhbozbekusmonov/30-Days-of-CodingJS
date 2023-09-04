5ef1c835512a44303d3043470745374a91f0f366
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-09-04 14:41:45 +0500

    ✅ gHappy done

--- Day14/index.js
+++ Day14/index.js
@@ -21,4 +21,25 @@ function equalIsNot(str) {
     return countIS === countNOT;
 }
 //-----------------------------------------------------------
+// gHappy('xxggxx') → true
+// gHappy('xxgxx') → false
+// gHappy('xxggyygxx') → false
+function gHappy(str) {
+    let happy = true;
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) === "g") {
+            if (i > 0 && str.charAt(i - 1) === "g") {
+                happy = true;
+            }
+            else if (i < str.length - 1 && str.charAt(i + 1) === "g") {
+                happy = true;
+            }
+            else {
+                happy = false;
+            }
+        }
+    }
+    return happy;
+}
+//---------------------------------------------------------------
 //# sourceMappingURL=index.js.map
--- Day14/index.ts
+++ Day14/index.ts
@@ -26,3 +26,25 @@ function equalIsNot(str: string): boolean {
 }
 
 //-----------------------------------------------------------
+
+// gHappy('xxggxx') → true
+// gHappy('xxgxx') → false
+// gHappy('xxggyygxx') → false
+
+function gHappy(str: string): boolean {
+    let happy: boolean = true;
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) === "g") {
+            if (i > 0 && str.charAt(i - 1) === "g") {
+                happy = true;
+            } else if (i < str.length - 1 && str.charAt(i + 1) === "g") {
+                happy = true;
+            } else {
+                happy = false;
+            }
+        }
+    }
+    return happy;
+}
+
+//---------------------------------------------------------------
