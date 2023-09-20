3912169f1ff70272f759b3d4cd33180cde7792a4
shakhbozbekusmonov <miracle_0319@bk.ru>
2023-09-20 16:33:17 +0500

    teenSum done

--- Day18/index.js
+++ Day18/index.js
@@ -9,4 +9,11 @@ function nearTen(num) {
     return num % 10 < 3 || num % 10 >= 8;
 }
 //----------------------------------------------------------------
+// teenSum(3, 4) → 7
+// teenSum(10, 13) → 19
+// teenSum(13, 2) → 19
+function teenSum(a, b) {
+    return (a >= 13 && a <= 19) || (b >= 13 && b <= 19) ? 19 : a + b;
+}
+//----------------------------------------------------------------
 //# sourceMappingURL=index.js.map
